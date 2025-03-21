dc3ac040b8951477c0825aee9df3e86278453219
ryugayeon <fbrkdus002@gmail.com>
2025-03-21 15:38:56 +0900

    매니폴드 객체 추가 중

--- Source/GoKill/Private/Mission.cpp
+++ Source/GoKill/Private/Mission.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Mission.h"
+#include "Components/ShapeComponent.h"
 
 // Sets default values
 AMission::AMission()
@@ -9,6 +10,12 @@ AMission::AMission()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	OverlapComp = CreateDefaultSubobject<UShapeComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetupAttachment(RootComponent);
+
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 }
 
 // Called when the game starts or when spawned
