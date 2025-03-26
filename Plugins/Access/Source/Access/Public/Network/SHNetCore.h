7adba13984c68f8d565905f30f9d9bc20734f71e
membersaccount <192158855+membersaccount@users.noreply.github.com>
2025-03-26 18:35:39 +0900

    [update] Add multiplay

--- Plugins/Access/Source/Access/Public/Network/SHNetCore.h
+++ Plugins/Access/Source/Access/Public/Network/SHNetCore.h
@@ -12,28 +12,47 @@
 
 #include "shCore.h"
 
+class USHNetGameInstance;
+class ASHNetPlayerController;
+
 class ACCESS_API SHNetCore
 {
 public:
 	SHNetCore();
+	SHNetCore(const SHNetCore&) = delete;
+	SHNetCore& operator=(const SHNetCore&) = delete;
 	~SHNetCore();
+
+	static SHNetCore* GetInstance();
 
 	bool Run();
 	void DestroyThread();
+
+	SOCKET* GetSocket();
+	void SetGameInstance(USHNetGameInstance* gameInstance_);
+	void SetController(ASHNetPlayerController* controller_);
 
 	void SendTestMsg();
 
 private:
 	void RecvThread();
 
+public:
+	Buffer::RecvBuffer recvBuffer;
+	Buffer::SendBuffer sendBuffer;
+
+	int SetGameInstanceCount = 0;
+
 private:
+	static SHNetCore* networkSystem;
+
 	SOCKET clientSocket;
 
 	std::thread recvThread;
 	std::atomic<bool> isRecvRun = true;
 
-	Buffer::RecvBuffer recvBuffer;
-	Buffer::SendBuffer sendBuffer;
+	USHNetGameInstance* cachedGameInstance = nullptr;
+	ASHNetPlayerController* cachedController = nullptr;
 
 	float testValue = 0.f;
 };
